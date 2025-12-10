3511b67ab6ab7f336dae6c8a01fa446c5f898a75
ayaannm12 <ayaanm5352@gmail.com>
2025-12-10 10:17:18 -0500

    aaa
    aa

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print(".")
+import random
+from time import *
+import requests
+
+
+def getGames(Games):
+    response = requests.get(f"https://videogamedb.uk/v3/api-docs/{Games.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+
+Games = getGames("Fortnite")
+print(Games)
+
