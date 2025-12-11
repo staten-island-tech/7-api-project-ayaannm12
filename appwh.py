a14ef3cdb61dcc276e56824cf6693c23f751d23b
ayaannm12 <ayaanm5352@gmail.com>
2025-12-11 10:17:22 -0500

    A

--- appwh.py
+++ appwh.py
@@ -1,21 +1,2 @@
-import random
-from time import *
-import requests
-
-BASE_SONGS = {
-    "Nomu": "https://genius.com/Good-kid-nomu-lyrics",
-    "Faster": "https://genius.com/Good-kid-faster-lyrics",
-    "Drifting": "https://genius.com/Good-kid-drifting-lyrics"
-}
-
-BASE_DESC = {
-    "Nomu": "https://en.wikipedia.org/wiki/Nomu_(song)",  
-    "Faster": "https://en.wikipedia.org/wiki/Faster_(Good_Kid_song)",
-    "Drifting": "https://en.wikipedia.org/wiki/Drifting_(Good_Kid_song)"
-}
-
-speling = "bad"
-
-
-
+print(">")
 
