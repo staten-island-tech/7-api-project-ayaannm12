7486aef0572bd930665a8b7de932fc33df4b19f1
ayaannm12 <ayaanm5352@gmail.com>
2025-12-16 09:55:19 -0500

    sssss
    ssss

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import random
 from time import *
 import requests
 
-CLOSE YOUR COMPUTER PLEASE
-def getGames(Games):
-    response = requ​ests.get(f"http​s://vidеogamedb.​uk/v3/a​pi-docs/​{Games.lower()}")
-    if response.s​tatus_code != 200:
-        print("Error fetching data !!!")
+
+def getgames(games):
+    response = requests.get(f"https://www.freetogame.com/api/{games.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
         return None
     
     
     data = response.json()
     return {
         "name": data["name"],
-        "types": [t["typе"]["name"] for t in data["types"]]
+        "types": [t["type"]["name"] for t in data["types"]]
     }
 
-Games = getGames("Fortnite BALLS")
-print(Games)
+games = getgames("Battlefield REDSEC")
+print(games)
