1c16969617578a20e372747d523c4e17df0fa521
ayaannm12 <ayaanm5352@gmail.com>
2025-12-17 10:16:43 -0500

    sr
    sr

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import random
 from time import *
 import requests
 
-   
 
 def getgames(games):
-    response = requests.get(f"https://www.freetogame.com/api/games?platform=pc/{games.lower()}")
+    response = requests.get(f"https://www.freetogame.com/api/games?platform=pc")
     if response.status_code != 200:
         print("Error fetching data!")
         print(response.json)
@@ -14,9 +13,9 @@ def getgames(games):
     
     data = response.json()
     return {
-        "name": data["name"],
-        "types": [t["type"]["name"] for t in data["types"]]
+        "title": data["title"],
+        "platform": [t["platform"]["title"] for t in data["platform"]]
     }
 
-games = getgames("pc")
+games = getgames("Overwatch 2")
 print(games)
