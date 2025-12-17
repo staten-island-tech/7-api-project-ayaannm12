eba3323db7bc91eddcb53b63a9732e7fc2d02216
ayaannm12 <ayaanm5352@gmail.com>
2025-12-17 11:38:03 -0500

    aaaa
    aaa

--- app.py
+++ app.py
@@ -3,19 +3,17 @@ from time import *
 import requests
 
 
-def getgames(games):
+def getgames():
     response = requests.get(f"https://www.freetogame.com/api/games?platform=pc")
     if response.status_code != 200:
         print("Error fetching data!")
         print(response.json)
         return None
-    
+    else:
+        wg1 =input("what Genre of Free Pc games do you wanna look at ?").lower().strip()
     
     data = response.json()
-    return {
-        "title": data["title"],
-        "platform": [t["platform"]["title"] for t in data["platform"]]
-    }
-
-games = getgames("Overwatch 2")
+    for game in data:
+        print(game["title"])
+games = getgames()
 print(games)
